74fba438257393f0b52fe3b7cd38b590ca0915da
tbehm <166077096+tbehm@users.noreply.github.com>
2024-05-24 23:56:27 -0300

    Delete eliminar_producto.php

--- eliminar_producto.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require "conexion.php";
-    $ubi=filter_input(INPUT_GET,'ubi');
-    $consulta=$conexion->prepare("DELETE FROM productos WHERE Id_producto=:ub");
-    $consulta->bindParam(":ub",$ubi);
-    $consulta->execute();
-    header('Location:listado_producto.php');
-?>
