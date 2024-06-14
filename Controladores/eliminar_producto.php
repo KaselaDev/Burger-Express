c9b88c2e7a85c1aae31ed0c7524b8f72c935e15c
tbehm <166077096+tbehm@users.noreply.github.com>
2024-06-14 13:00:20 -0300

    Delete Controladores directory

--- Controladores/eliminar_producto.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require "../Modelos/consultas.php";
-    $ubi=filter_input(INPUT_GET,'ubi');
-    delete_producto($ubi);
-    header('Location:../Vistas/listado_producto.php');
-?>
--- Controladores/realizar_pedido.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	require '../Modelos/consultas.php';
-
-	$mesa=filter_input(INPUT_GET, 'ubi');
-	echo $mesa;
-
-	estado_mesa(1,$mesa,"ocupada");
-	header("Location:../Vistas/user/menu_pedidos.php");
- ?>
